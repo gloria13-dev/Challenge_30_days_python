53cb436762b6de3aa52ca1f7063c4eca35ac61ac
KPOGO Yayra Gloria <kpogoyayra0@gmail.com>
2025-08-08 15:37:09 +0000

    day 17
    Signed-off-by: KPOGO Yayra Gloria <kpogoyayra0@gmail.com>

---
+++ jour17.py
@@ -0,0 +1,7 @@
+#challenge python
+#Exercises: day 17
+
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+*nordic_countries , es, ru = names
+
+print(f'{nordic_countries = }, \t{es = }, \t{ru = }')
